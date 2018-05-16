ed269be8beebd4f6f14da75a4d0fa935d5c00199
Philine Wairata <philinewairata@wcw-stud-145-109-34-174.wireless.uva.nl>
2018-05-16 11:26:32 +0200

    Initial Commit

---
+++ trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trivia
+//
+//  Created by Philine Wairata on 16/05/2018.
+//  Copyright © 2018 Philine Wairata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
